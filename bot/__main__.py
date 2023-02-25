f7ecb3f7363552f607dea8cef1e5eb1282a2cc8e
Eremetko Valentin <32tut32@gmail.com>
2023-02-25 09:42:07 +0300

    initial commit

---
+++ bot/__main__.py
@@ -0,0 +1,29 @@
+import asyncio
+import os
+import logging
+from aiogram import Dispatcher, Bot
+from commands import register_user_commands
+
+
+async def main() -> None:
+
+    token = os.getenv('TELEGRAM_TOKEN_API')
+
+    logging.basicConfig(level=logging.DEBUG)
+
+    dispatcher = Dispatcher()
+    bot = Bot(
+        token
+    )
+
+    register_user_commands(
+        dispatcher
+    )
+
+    await dispatcher.start_polling(bot)
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except(KeyboardInterrupt, SystemExit):
+        print('Bot stopped')
