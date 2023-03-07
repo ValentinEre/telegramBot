393caca6464b5002158298ed62ee244b2b58b0b4
Eremetko Valentin <32tut32@gmail.com>
2023-03-07 20:33:14 +0300

    new dependency file

--- tests/test_handler/test_download_images.py
+++ tests/test_handler/test_download_images.py
@@ -5,19 +5,15 @@ from icrawler.builtin import GoogleImageCrawler
 
 @pytest.mark.asyncio
 def test_download_images():
-    # Define search query and number of images to download
     query = "dogs"
     num_images = 3
 
-    # Download images using icrawler
-    output_dir = "../test_images"
+    output_dir = "/test_images"
     crawler = GoogleImageCrawler(storage={"root_dir": output_dir})
     crawler.crawl(keyword=query, max_num=num_images)
 
-    # Verify that at least one image was downloaded successfully
     images = os.listdir(output_dir)
     assert len(images) > 0
 
-    # Clean up test artifacts
     for image in images:
         os.remove(os.path.join(output_dir, image))
