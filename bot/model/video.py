c7b9f9acf73c71621150acf0ce55de55d71678a3
Eremetko Valentin <32tut32@gmail.com>
2023-03-27 14:19:35 +0300

    New theme

--- bot/model/video.py
+++ bot/model/video.py
@@ -9,26 +9,78 @@ from moviepy.editor import *
 class Video:
 
     def __init__(self):
-        key_words = ["Tulips-flowers", "Orchids-flowers", "Lilies-flowers", "Lotus-flower", "Dahlia-flower",
-                     "Apple-blossom", "lily-flower", "Snapdragons-flowers", "Aster-flower", "Violet-flower",
-                     "Waterfall", "Ghost-Orchid", "Titan-Arum", "Pitcher-Plant", "Black-Bat-Flower",
-                     "Corpse-Lily", "Kokia-Cookei", "Jade-Vine", "Venus-Flytrap", "Rafflesia-Arnoldii",
-                     "Chocolate-Cosmos", "Gladioli-flower", "CherryTree", "PineTree", "Mountain", "River", "Sun"]
-        self.keyWord = random.choice(key_words)
+
+        # Список игр и тем
+        games = {
+            "Minecraft": [
+                "Локации и карты",
+                "Строительство и дизайн",
+                "Создание и прокачка персонажей",
+                "Мобы и животные",
+                "Крафтинг и ресурсы",
+                "Фан-арт и косплей"
+            ],
+            "Fortnite": [
+                "Скины и косметические предметы",
+                "Локации и карты",
+                "Оружие и снаряжение",
+                "Персонажи и их способности",
+                "События и праздники, связанные с игрой",
+                "Награды и достижения в игре"
+            ],
+            "PUBG": [
+                "Локации и карты",
+                "Оружие и снаряжение",
+                "Персонажи и их способности",
+                "Награды и достижения в игре",
+                "Квесты и миссии",
+                "Фан-арт и косплей"
+            ],
+            "Call of Duty: Mobile": [
+                "Локации и карты",
+                "Оружие и снаряжение",
+                "Персонажи и их способности",
+                "Награды и достижения в игре",
+                "Квесты и миссии",
+                "События и праздники, связанные с игрой"
+            ],
+            "Clash of Clans": [
+                "Строительство и дизайн",
+                "Создание и прокачка персонажей",
+                "Награды и достижения в игре",
+                "Квесты и миссии",
+                "События и праздники, связанные с игрой",
+                "Фан-арт и косплей"
+            ],
+            "Among Us": [
+                "Персонажи и их способности",
+                "Локации и карты",
+                "Фан-арт и косплей",
+                "События и праздники, связанные с игрой",
+                "Награды и достижения в игре",
+                "Моды и дополнения"
+            ],
+            "Roblox": [
+                "Создание и прокачка персонажей",
+                "Локации и карты",
+                "Мини-игры и развлечения",
+                "События и праздники, связанные с игрой",
+                "Награды и достижения в игре",
+                "Фан-арт и косплей"
+            ]
+        }
+
+        game = random.choice(list(games.keys()))
+        theme = random.choice(games[game])
+
+        self.game_theme = f"${game}_{theme}"
+        self.topic = f"${game} {theme}"
+
         self.path_to_pic = "/home/valentin/Изображения/pictures"
         self.path_to_video = "/home/valentin/Видео/vid"
         self.path_to_audio = "/home/valentin/Музыка/music"
         self.path_to_resize_pic = "/home/valentin/Изображения/pic_1080x1920"
-        self.random_num = random.randrange(10000)
-
-    @staticmethod
-    def get_random_types():
-        types = ["spring", "summer", "marvelous", "winter", "sunset", "indescribable", "unusual", "austria",
-                 "autumn", "landscape", "norwegian", "norway", "france", "japan", "snowy", "sunrise"]
-        return random.choice(types)
-
-    def new_sentence(self):
-        return self.keyWord + " " + self.get_random_types()
+        self.random_num = random.randrange(77777)
 
     def get_pic(self):
         google_crawler = GoogleImageCrawler(
@@ -38,7 +90,7 @@ class Video:
             type="photo",
             license="commercial,modify"
         )
-        google_crawler.crawl(keyword=self.new_sentence(), filters=filters, offset=0, max_num=12, file_idx_offset=0)
+        google_crawler.crawl(self.topic, filters=filters, offset=0, max_num=12, file_idx_offset=0)
 
     def resize_pic(self):
         print("Now resize")
@@ -48,7 +100,7 @@ class Video:
         for pic in images:
             image = Image.open(f"{pic}")
             new_image = image.resize((1600, 1920))
-            new_image.save(f'{self.path_to_resize_pic + "/" + self.keyWord + str(pic_num)}.jpg')
+            new_image.save(f'{self.path_to_resize_pic + "/" + self.game_theme + str(pic_num)}.jpg')
             pic_num += 1
 
     def get_clip(self):
@@ -65,7 +117,7 @@ class Video:
             final_clip = concatenate_videoclips(clips=clip, method="compose") \
                 .fadein(1).fadeout(1).set_audio(audio_file)
             final_clip.write_videofile(
-                f"{dir_for_video}/{self.keyWord}_{self.random_num}.mp4", fps=30)
+                f"{dir_for_video}/{self.game_theme}_{self.random_num}.mp4", fps=30)
 
     def remove_pic(self):
         dir_pic_default = f"{self.path_to_pic}"
